c56dc5f9b38f49d1f1af6d605f0fba0d7afc92b6
Safal Lama <140276215+RyuZinOh@users.noreply.github.com>
2024-11-07 17:48:47 +0545

    client addition || rect routering

---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./Footer.js";
+import Header from "./Header.js";
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      <main style={{ minHeight: "85vh" }}>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
